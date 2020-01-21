f996feb553eb539eaa8428b0491d1b885d19065a
Adriano Chaar <adrianoschaar@hotmail.com>
2020-01-21 16:34:01 -0300

    publish website

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express'); 
+var app = express();
+                     
+app.get("/", (req, res) =>{
+    res.send("<h1>Hello World</h1>")
+})
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
+});
